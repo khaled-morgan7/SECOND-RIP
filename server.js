dc7a48543a061e20e7698ac3cfbb349685c72131
khaled-morgan7 <145543723+khaled-morgan7@users.noreply.github.com>
2024-12-14 23:08:28 +0200

    PART 2 SERVER

--- server.js
+++ server.js
@@ -29,3 +29,64 @@ const verifyToken = (req, res, next) => {
         next()
     })
 }
+server.post('/user/register', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const pass = req.body.pass;
+
+    // Hash the password
+    bcrypt.hash(pass, 10, (err, hashedPassword) => {
+        if (err) {
+            return res.status(500).send('Error hashing password');
+        }
+
+        // Query to insert the new user
+        const query = `INSERT INTO USER (NAME, EMAIL, PASS, ISADMIN) VALUES (?, ?, ?, ?)`;
+
+        // Execute the query
+        db.run(query, [name, email, hashedPassword, 0], (err) => {
+            if (err) {
+                return res.status(401).send(err.message);
+            } else {
+                return res.status(200).send('Registration successful');
+            }
+        });
+    });
+});
+
+server.post('/user/login', (req, res) => {
+    const email = req.body.email
+    const password = req.body.pass
+
+    // Fetch user details from the database using parameterized queries to prevent SQL injection
+    db.get(`SELECT * FROM USER WHERE EMAIL = ?`, [email], (err, row) => {
+        if (err || !row) {
+            return res.status(401).send('Wrong email or password')
+        }
+
+        bcrypt.compare(password, row.PASS, (err, isMatch) => {
+            if (err) {
+                return res.status(500).send('Error comparing password')
+            }
+
+            if (!isMatch) {
+                return res.status(401).send('Invalid credentials')
+            } else {
+                // Generate JWT token on successful login
+                const userID = row.ID
+                const isAdmin = row.ISADMIN
+                const token = generateToken(userID, isAdmin)
+
+                // Send token as a cookie to the client for authentication
+                res.cookie('authToken', token, {
+                    httpOnly: true,
+                    sameSite: 'none',
+                    secure: true,
+                    expiresIn: '1h'
+                })
+
+                return res.status(200).json({ id: userID, admin: isAdmin })
+            }
+        })
+    })
+})
